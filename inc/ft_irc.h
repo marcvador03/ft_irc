7e31eff0e58eb6f373307e904c01f7df1a2aae76
Marc Fleury <mfleury03@mailo.com>
2025-07-03 21:29:24 +0200

    corrected include

--- inc/ft_irc.h
+++ inc/ft_irc.h
@@ -6,12 +6,12 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 19:25:37 by mfleury           #+#    #+#             */
-/*   Updated: 2025/06/19 15:51:51 by mpietrza         ###   ########.fr       */
+/*   Updated: 2025/07/02 17:06:17 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_IRC_H
 # define FT_IRC_H
-
 # include <iostream>
 # include <string>
 # include "Server.hpp"

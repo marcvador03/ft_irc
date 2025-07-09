cd150c87a280e6d32069439eb06cb3aab22df5b0
Marc Fleury <mfleury03@mailo.com>
2025-07-09 15:37:24 +0200

    merge with mpietrza Channel version

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,12 +6,16 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 15:13:42 by mpietrza          #+#    #+#             */
-/*   Updated: 2025/07/08 18:25:07 by mpietrza         ###   ########.fr       */
+/*   Updated: 2025/07/09 15:36:47 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
+# include <iostream>
+# include <map>
+# include <iterator>
+# include "Client.hpp"
 
 #include <string>
 #include <set>
@@ -24,33 +28,28 @@
 - Forward messages to all users in channel.  
 - Implement commands like `KICK`, `INVITE`, `TOPIC`, `MODE`.  
  */
-class Channel
-{
-	private:
-		std::string			_name;			//channel name
-		std::string			_topic;			//channel topic
-		std::set<Client *>	_members;		//all clients in channel
-		std::set<Client *>	_operators;		//channel operators (for mode 'o')
-		bool				_inviteOnly;	//mode 'i' - new members can only join when inviteed by other users (true) / everyone can join (false)
-		bool				_topicLocked;	//mode 't' - topic of the channel can (false) or cannot be changed (true)
-		bool				_hasKey;		//mode 'k' - the channel has key (password) (true) or doesn't have it (false)
-		std::string			_key;			//mode 'k''s key (password) storage
-		unsigned int		_limit;			//mode 'l' - the channel doesn't have any limit of users (0) or has a limit of users within the range of unsigned int (1 - 4,294,967,295)
 
+class Client;
+
+class Channel {
 	public:
-		//Constructor and destructor
-		Channel(const std::string &name);
-		~Channel();
+		/* Coplien form */	
+		Channel ( std::string name, std::string topic );
+		virtual ~Channel ( void );
 
-		//basic getters
-		const std::string &getName() const;
+		void	addClient( int slot, Client *c);
+		void	removeClient( int slot );
 		
+		/*Getters and setters */
+		std::string	getName( void ) const;
+		
+		//*************to revist***********************//
 		//invite only related
 		void setInviteOnly(bool isInviteOnly);
 		bool isInviteOnly() const;
 
 		//topic related
-		const std::string &getTopic() const;
+		std::string getTopic() const;
 		void setTopic(const std::string &topic);
 		void setTopicLocked(bool isLocked);
 		bool isTopicLocked() const;
@@ -72,6 +71,28 @@ class Channel
 		bool addOperator(Client *client);
 		bool removeOperator(Client *client);
 		bool isOperator(Client *client);
+		
+		/* Exceptions messages */
+		class ErrnoException: public std::exception {
+			public:
+				virtual const char* what() const throw()
+				{ return std::strerror(errno); }
+		};
+	private:
+		/* Coplien form - unauthorized constructors */	
+		Channel ( const Channel &other );
+		Channel &operator-( const Channel &other );
+		
+		std::map <int, Client *>	_clients;
+		std::map <int, Client *>	_operators;		//channel operators (for mode 'o')
+
+		std::string			_name;			//channel name
+		std::string			_topic;			//channel topic
+		bool				_inviteOnly;	//mode 'i' - new members can only join when inviteed by other users (true) / everyone can join (false)
+		bool				_topicLocked;	//mode 't' - topic of the channel can (false) or cannot be changed (true)
+		bool				_hasKey;		//mode 'k' - the channel has key (password) (true) or doesn't have it (false)
+		std::string			_key;			//mode 'k''s key (password) storage
+		unsigned int		_limit;			//mode 'l' - the channel doesn't have any limit of users (0) or has a limit of users within the range of unsigned int (1 - 4,294,967,295)
 };
 
 #endif
